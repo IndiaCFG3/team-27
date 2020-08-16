6fb8427d0b8b115e97a03530c97916b0b42e46c7
AnnanyaV <annanyaved.07@gmail.com>
2020-08-16 20:47:02 +0530

    Added all APIs with GET header

--- flaskapp/app/routes.py
+++ flaskapp/app/routes.py
@@ -15,72 +15,83 @@ def index():
 
 @app.route('/eggsDemand')
 def eggsDemand():
+   
     if request.method=='GET':
+        country = request.headers.get('country')
         data= pd.read_csv('eggs_demand.csv')
-        data= data.groupby('Area').agg({'Year': lambda x: list(x),'Value': lambda x: list(x)}).to_json(orient='index')
-        return json.dumps(data)
+        data= data.groupby('Area').agg({'Year': lambda x: list(x),'Value': lambda x: list(x)}).to_dict(orient='index')
+        return json.dumps(data[country])
 
 @app.route('/eggsPrice')
 def eggsPrice():
     if request.method=='GET':
+        country = request.headers.get('country')
         data= pd.read_csv('eggs_price_demand.csv')
-        data= data.groupby('Area').agg({'Year': lambda x: list(x),'Value': lambda x: list(x)}).to_json(orient='index')
-        return json.dumps(data)
+        data= data.groupby('Area').agg({'Year': lambda x: list(x),'Value': lambda x: list(x)}).to_dict(orient='index')
+        return json.dumps(data[country])
 
 @app.route('/eggsMalnutrition')
 def eggsMalnutrition():
     if request.method=='GET':
+        country = request.headers.get('country')
         data= pd.read_csv('eggs_malnutrition_demand.csv')
         data= data.groupby('Counrty ').agg({'Survey year': lambda x: list(x),'Wasting': lambda x: list(x), 'Stunting': lambda x: list(x), 
-        'Underweight': lambda x: list(x)}).to_json(orient='index')
-        return json.dumps(data)
+        'Underweight': lambda x: list(x)}).to_dict(orient='index')
+        return json.dumps(data[country])
 
 @app.route('/eggsProtein')
 def eggsProtein():
     if request.method=='GET':
+        country = request.headers.get('country')
         data= pd.read_csv('eggs_malnutrition_demand.csv')
         data= data.groupby('Counrty ').agg({'Survey year': lambda x: list(x),'Wasting': lambda x: list(x), 'Stunting': lambda x: list(x), 
-        'Underweight': lambda x: list(x)}).to_json(orient='index')
-        return json.dumps(data)
+        'Underweight': lambda x: list(x)}).to_dict(orient='index')
+        return json.dumps(data[country])
 
 @app.route('/eggsProducedByArea')
 def eggsProducedByArea():
     if request.method=='GET':
+        country = request.headers.get('country')
         data= pd.read_csv('eggs_produced.csv')
-        data= data.groupby('Area').agg({'Year': lambda x: list(x),'Value': lambda x: list(x)}).to_json(orient='index')
-        return json.dumps(data)
+        data= data.groupby('Area').agg({'Year': lambda x: list(x),'Value': lambda x: list(x)}).to_dict(orient='index')
+        return json.dumps(data[country])
 
 @app.route('/eggsProducedByYear')
 def eggsProducedByYear():
     if request.method=='GET':
+        country = request.headers.get('country')
         data= pd.read_csv('eggs_produced.csv')
-        data= data.groupby('Year').agg({'Area': lambda x: list(x),'Value': lambda x: list(x)}).to_json(orient='index')
-        return json.dumps(data)
+        data= data.groupby('Year').agg({'Area': lambda x: list(x),'Value': lambda x: list(x)}).to_dict(orient='index')
+        return json.dumps(data[country])
 
 @app.route('/eggsExportedByArea')
 def eggsExportedByArea():
     if request.method=='GET':
+        country = request.headers.get('country')
         data= pd.read_csv('eggs_export.csv')
-        data= data.groupby('Area').agg({'Year': lambda x: list(x),'Value': lambda x: list(x)}).to_json(orient='index')
-        return json.dumps(data)
+        data= data.groupby('Area').agg({'Year': lambda x: list(x),'Value': lambda x: list(x)}).to_dict(orient='index')
+        return json.dumps(data[country])
 
 @app.route('/eggsExportedByYear')
 def eggsExportedByYear():
     if request.method=='GET':
+        country = request.headers.get('country')
         data= pd.read_csv('eggs_export.csv')
-        data= data.groupby('Year').agg({'Area': lambda x: list(x),'Value': lambda x: list(x)}).to_json(orient='index')
-        return json.dumps(data)
+        data= data.groupby('Year').agg({'Area': lambda x: list(x),'Value': lambda x: list(x)}).to_dict(orient='index')
+        return json.dumps(data[country])
 
 @app.route('/eggsProducerPriceByArea')
 def eggsProducerPriceByArea():
     if request.method=='GET':
+        country = request.headers.get('country')
         data= pd.read_csv('eggs_price_demand.csv')
-        data= data.groupby('Area').agg({'Year': lambda x: list(x),'Value': lambda x: list(x)}).to_json(orient='index')
-        return json.dumps(data)
+        data= data.groupby('Area').agg({'Year': lambda x: list(x),'Value': lambda x: list(x)}).to_dict(orient='index')
+        return json.dumps(data[country])
 
 @app.route('/eggsProducerPriceByYear')
 def eggsProducerPriceByYear():
     if request.method=='GET':
+        country = int(request.headers.get('country'))
         data= pd.read_csv('eggs_price_demand.csv')
-        data= data.groupby('Year').agg({'Area': lambda x: list(x),'Value': lambda x: list(x)}).to_json(orient='index')
-        return json.dumps(data)
+        data= data.groupby('Year').agg({'Area': lambda x: list(x),'Value': lambda x: list(x)}).to_dict(orient='index')
+        return json.dumps(data[country])
