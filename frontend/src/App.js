d0338d390d0e608b735dfd4cc4842811e3dbfa40
rachit2501 <rac.sri25@gmail.com>
2020-08-16 19:29:23 +0530

    sdf

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,13 @@ import "./App.css";
 import Container from "@material-ui/core/Container";
 import Top from "./containers/Top";
 import Demand from "./containers/Demand";
+import { Router } from "@reach/router";
+import Waste from "./containers/Waste";
 
 function App() {
   const [country, updateCountry] = useState("");
   const [year, updateYear] = useState("");
-
-  return (
+  const ContainerC = () => (
     <Container maxWidth="lg">
       <div>
         <Top
@@ -26,6 +27,12 @@ function App() {
         />
       </div>
     </Container>
+  );
+  return (
+    <Router>
+      <ContainerC path="/" default />
+      <Waste path="/width" />
+    </Router>
   );
 }
 
